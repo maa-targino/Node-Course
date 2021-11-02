cb38dae6ed3bfb194d8fda70480343fc1746c5ed
Marco Targino <maa.targino@gmail.com>
2021-11-01 21:04:30 -0300

    intalled ejs

--- index.js
+++ index.js
@@ -1,20 +1,18 @@
-const express = require('express');
-const app = express(); //Inicia o express
+const express = require('express')
+const app = express()
 
-app.get('/home', function(req,res){
-    console.log('GET required')
-    res.send('<h1>Welcome to our home page!</h1>')
+app.set('view engine', 'ejs')
+
+app.get('/:name?/:lang?', (req,res) => {
+    var name = req.params.name
+    var lang = req.params.lang
+    
+    res.render("index.ejs",{
+        name: name,
+        lang: lang,
+    })
 })
 
-app.get('/contact', function(req,res){
-    console.log('GET required')
-    res.send('<h1>Welcome to our contact page!</h1><h2>Please leave a message.</h2>')
-})
-
-app.listen(3000, function(erro){
-    if(erro){
-        console.log("An error has ocurred!")
-    }else{
-        console.log("Server initialized successfully!")
-    }
+app.listen(3030, () => {
+    console.log('Running')
 })
