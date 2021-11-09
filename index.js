6a0c5c999dd21a01436cda6655333cf103168159
Marco Targino <maa.targino@gmail.com>
2021-11-09 12:33:51 -0300

    update

--- index.js
+++ index.js
@@ -8,6 +8,10 @@ app.use(express.static('public'))
 app.get('/', (req,res) => {
     res.render("index.ejs")
 })
+
+app.get('/form', (req,res) => {
+    res.render("form.ejs")
+})
  
 app.listen(3030, () => {
     console.log('Running')
